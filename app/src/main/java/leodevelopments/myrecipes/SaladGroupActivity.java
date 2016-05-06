74dfaed70549f593cda66e380deb43e7baed0a72
Флоров Леонид <l.florov@kupivip.local>
2016-05-06 17:58:12 +0300

    Добавил новый класс и активи для групп "Салаты".

---
+++ app/src/main/java/leodevelopments/myrecipes/SaladGroupActivity.java
@@ -0,0 +1,9 @@
+package leodevelopments.myrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by l.florov on 06.05.2016.
+ */
+public class SaladGroupActivity extends AppCompatActivity {
+}
