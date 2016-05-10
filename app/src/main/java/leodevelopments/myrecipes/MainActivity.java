e94ff0f08bb92cb9053acac810d0966a4b057b35
Флоров Леонид <l.florov@kupivip.local>
2016-05-10 19:02:06 +0300

    Причесал код 2. Добавил обработчки кнопки на "Салаты". Теперь открывается активити категории "Салаты". Добавлю остальные обработчики только после того, как пройду всю ветвь создания рецепта.

--- app/src/main/java/leodevelopments/myrecipes/MainActivity.java
+++ app/src/main/java/leodevelopments/myrecipes/MainActivity.java
@@ -1,5 +1,6 @@
 package leodevelopments.myrecipes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
     DrawerLayout mDrawerLayout;
@@ -21,22 +23,21 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         /**
-         *Setup the DrawerLayout and NavigationView
+         *Определяем DrawerLayout и NavigationView
          */
 
              mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
              mNavigationView = (NavigationView) findViewById(R.id.shitstuff) ;
 
         /**
-         * Lets inflate the very first fragment
-         * Here , we are inflating the TabFragment as the first Fragment
+         * "Надуваем" первый таб
          */
 
              mFragmentManager = getSupportFragmentManager();
              mFragmentTransaction = mFragmentManager.beginTransaction();
              mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
         /**
-         * Setup click events on the Navigation View Items.
+         * Прописываем нажатия бокового меню
          */
 
              mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -44,18 +45,22 @@ public class MainActivity extends AppCompatActivity{
              public boolean onNavigationItemSelected(MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
 
-
-
-                 if (menuItem.getItemId() == R.id.nav_item_sent) {
+                 if (menuItem.getItemId() == R.id.categories_menu_button) {
                      FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                     fragmentTransaction.replace(R.id.containerView,new AddRecipeActivity()).commit();
+                     fragmentTransaction.replace(R.id.containerView,new CategoriesTab()).commit();
 
                  }
 
-                if (menuItem.getItemId() == R.id.nav_item_inbox) {
-                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
-                }
+                 if (menuItem.getItemId() == R.id.favorites_menu_button) {
+                     FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                     fragmentTransaction.replace(R.id.containerView,new FavouritesTab()).commit();
+
+                 }
+
+//                if (menuItem.getItemId() == R.id.nav_item_inbox) {
+//                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+//                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+//                }
 
                  return false;
             }
@@ -63,7 +68,7 @@ public class MainActivity extends AppCompatActivity{
         });
 
         /**
-         * Setup Drawer Toggle of the Toolbar
+         * Определяем переключатель тулбара
          */
 
                 android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
@@ -75,4 +80,8 @@ public class MainActivity extends AppCompatActivity{
                 mDrawerToggle.syncState();
 
     }
+    public void saladCatButton(View view){
+            Intent intent = new Intent(MainActivity.this, SaladGroupActivity.class);
+            startActivity(intent);
+        }
 }
