0e3d71e117a6db0744a71270bbb65f5fd097f86b
Флоров Леонид <l.florov@kupivip.local>
2016-05-31 20:59:11 +0300

    Сделал Bottom Navigation Bar, но он мне не очень нравится, потому что не скроллится вниз и приложение падает при нажатии на кнопку бара.
    После чего откатился назад.
    Надо всё заново делать.

--- app/src/main/java/leodevelopments/myrecipes/MainActivity.java
+++ app/src/main/java/leodevelopments/myrecipes/MainActivity.java
@@ -13,8 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity{
     DrawerLayout mDrawerLayout;
